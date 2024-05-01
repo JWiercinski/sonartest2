c26406aeb48f834b04a2db0344a97041f4d474ae
JWiercinski <jacek.wiercinski@student.uj.edu.pl>
2024-05-01 17:33:38 +0200

    Refaktor modułu basket

--- reactclient/src/Basket.js
+++ reactclient/src/Basket.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState, useMemo} from 'react';
 import {Link} from "react-router-dom";
+
 export const BasketContext = React.createContext();
+
 export const BasketProvider = ({ children }) => {
-    const [basket, setBasket] = React.useState(() => {
+    const [basket, setBasket] = useState(() => {
         // Try to get the basket from localStorage
         const savedBasket = localStorage.getItem('basket');
         if (savedBasket) {
@@ -10,11 +12,15 @@ export const BasketProvider = ({ children }) => {
         }
         return [];
     });
+
     React.useEffect(() => {
         localStorage.setItem('basket', JSON.stringify(basket));
     }, [basket]);
+
+    const value = useMemo(() => ({ basket, setBasket }), [basket, setBasket]);
+
     return (
-        <BasketContext.Provider value={{ basket, setBasket }}>
+        <BasketContext.Provider value={value}>
             {children}
         </BasketContext.Provider>
     );
@@ -22,10 +28,12 @@ export const BasketProvider = ({ children }) => {
 
 function Basket () {
     const { basket } = React.useContext(BasketContext);
+
     const clearBasket = () => {
-        localStorage.removeItem("basket")
-        window.location.reload()
+        localStorage.removeItem("basket");
+        window.location.reload();
     }
+
     const total = basket.reduce((accumulator, item) => {
         return accumulator + (item.product.PRICE * item.quantity);
     }, 0);
@@ -36,18 +44,17 @@ function Basket () {
             <Link to="/products"><button>Kontynuuj zakupy</button></Link>
             <h1>Your Basket:</h1>
             {basket.map((item) => (
-                <div key={item.index}>
+                <div key={item.product.ID}>
                     <h2>{item.product.NAME}</h2>
                     <p>Cena jednostkowa: {item.product.PRICE} PLN</p>
                     <p>Ilość: {item.quantity}</p>
                 </div>
             ))}
-           <h2>Podsuma: {total} PLN</h2>
-            <button style={{backgroundColor:"darkred", color:"white"}} onClick={() => clearBasket()}>Wyczyść koszyk</button>
+            <h2>Podsuma: {total} PLN</h2>
+            <button style={{backgroundColor:"darkred", color:"white"}} onClick={clearBasket}>Wyczyść koszyk</button>
             <Link to="/payments"><button>Przejdź do płatności</button></Link>
-            <p></p>
         </div>
     );
-};
+}
 
-export default Basket
+export default Basket;
